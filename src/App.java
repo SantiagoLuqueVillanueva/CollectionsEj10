ca2bd6433fe734f19fa8ee4271fa277b4c86cc13
Santiago <luquevillanuevasantiago@gmail.com>
2026-02-26 12:14:50 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HashMap<String, String> diccionario = new HashMap<>();
+
+        diccionario.put("gato", "cat");
+        diccionario.put("perro", "dog");
+        diccionario.put("agua", "water");
+        diccionario.put("fuego", "fire");
+        diccionario.put("tierra", "earth");
+        diccionario.put("aire", "air");
+        diccionario.put("sol", "sun");
+        diccionario.put("luna", "moon");
+        diccionario.put("estrella", "star");
+        diccionario.put("cielo", "sky");
+        diccionario.put("ordenador", "computer");
+        diccionario.put("teclado", "keyboard");
+        diccionario.put("raton", "mouse");
+        diccionario.put("pantalla", "screen");
+        diccionario.put("libro", "book");
+        diccionario.put("mesa", "table");
+        diccionario.put("silla", "chair");
+        diccionario.put("puerta", "door");
+        diccionario.put("ventana", "window");
+        diccionario.put("coche", "car");
+
+        String palabraUsuario = System.console().readLine();
+
+        if (diccionario.containsKey(palabraUsuario)) {
+            String traduccion = diccionario.get(palabraUsuario);
+            System.out.println("La traducción de '" + palabraUsuario + "' es: " + traduccion);
+        } else {
+            System.out.println("Lo siento, la palabra '" + palabraUsuario + "' no está en el diccionario.");
+        }
     }
 }
